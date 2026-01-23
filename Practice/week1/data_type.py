d4e2af9e7133e83d35e31cf11025e2be6179c387
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-01-23 14:39:00 +0500

    add data_type.py in week1

---
+++ Practice/week1/data_type.py
@@ -0,0 +1,31 @@
+x = 12345j
+q = 35
+w = "print"
+e = 35.5
+r = ["ff", "bs", "pp"]
+t = ("gen", "male", "f")
+y = range(q)
+u = {44,55,00}
+i = {"name":"Myrzabek", "age":17}
+o = frozenset({"apple", "banana", "cherry"})
+p = False
+a = b"My"
+s = bytearray(8)
+d = memoryview(bytes(5))
+f = None
+
+print(type(x))
+print(type(q))
+print(type(w))
+print(type(e))
+print(type(r))
+print(type(t))
+print(type(y))
+print(type(u))
+print(type(i))
+print(type(o))
+print(type(p))
+print(type(a))
+print(type(s))
+print(type(d))
+print(type(f))
