c324113934168a7af9d10da5b4145ecedc5736db
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-02-05 15:26:40 +0500

    [week2]-add_Booleans_as_Comparison_Results.py

---
+++ Practice/week2/Booleans_as_Comparison_Results.py
@@ -0,0 +1,10 @@
+list_a = [10, 20, 30]
+list_b = [10, 20, 30]
+
+list_c = list_a
+
+print(f"list_a == list_b: {list_a == list_b}")  
+
+print(f"list_a is list_b: {list_a is list_b}")  
+
+print(f"list_a is list_c: {list_a is list_c}")  
