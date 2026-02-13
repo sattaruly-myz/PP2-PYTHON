631c8242774a2dd24a129bceaaed0c0359a12dfc
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-02-13 18:17:23 +0500

    [inheritance]-update_inheritance_basics.py

--- Practice/week3/inheritance/inheritance_basics.py
+++ Practice/week3/inheritance/inheritance_basics.py
@@ -1,11 +1,16 @@
-class Animal:
-    def speak(self):
-        print("Animal makes a sound")
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
 
+    def work(self):
+        print(f"{self.name} works")
 
-class Dog(Animal):
+class Manager(Employee):
     pass
 
+class Developer(Employee):
+    pass
 
-d = Dog()
-d.speak()
+m = Manager("Myrzabek", 50000)
+d = Developer("CR7", 60000)
