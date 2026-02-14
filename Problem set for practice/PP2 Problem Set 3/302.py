c329636b2064b34782317b7d5caecaefd065bb41
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-02-14 15:36:19 +0500

    [PS2]-add_302.py

---
+++ Problem set for practice/PP2 Problem Set 3/302.py
@@ -0,0 +1,14 @@
+def isUsual(num):
+    while num % 2 == 0:
+        num //= 2
+    while num % 3 == 0:
+        num //= 3
+    while num % 5 == 0:
+        num //= 5
+    return num == 1
+
+n = int(input())
+if isUsual(n):
+    print("Yes")
+else:
+    print("No")
