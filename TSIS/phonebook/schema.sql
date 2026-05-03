749a414c123d01fd7a308961e4adaf4439e8912f
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-05-03 08:57:31 +0500

    [phonebook]-schema.sql

---
+++ TSIS/phonebook/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS groups (
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+INSERT INTO groups (name)
+VALUES ('Family'), ('Work'), ('Friend'), ('Other')
+ON CONFLICT (name) DO NOTHING;
+
+ALTER TABLE contacts
+    ADD COLUMN IF NOT EXISTS email    VARCHAR(100),
+    ADD COLUMN IF NOT EXISTS birthday DATE,
+    ADD COLUMN IF NOT EXISTS group_id INTEGER REFERENCES groups(id);
+
+CREATE TABLE IF NOT EXISTS phones (
+    id         SERIAL PRIMARY KEY,
+    contact_id INTEGER REFERENCES contacts(id) ON DELETE CASCADE,
+    phone      VARCHAR(20) NOT NULL,
+    type       VARCHAR(10) CHECK (type IN ('home', 'work', 'mobile'))
+);
