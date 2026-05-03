9cc7aef089d6b57d79f2895b8914e48156ec01b8
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-05-03 12:18:53 +0500

    [Snake]-add_config.py

---
+++ TSIS/Snake/config.py
@@ -0,0 +1,10 @@
+WINDOW_W = 800
+WINDOW_H = 640
+HUD_H = 40
+GRID = 20
+COLS = WINDOW_W // GRID
+ROWS = (WINDOW_H - HUD_H) // GRID
+FPS = 60
+BASE_SPEED = 8
+FOOD_PER_LEVEL = 5
+DB_DSN = "host=localhost port=5432 dbname=snake_game user=postgres password=postgres"
